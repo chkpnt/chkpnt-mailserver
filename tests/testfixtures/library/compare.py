300be3c800aff7875df67f7df2a83dfb5da730da
Gregor Dschung <gregor@chkpnt.de>
2019-04-25 00:47:32 +0200

    Refactor compare.py towards OOP

--- tests/testfixtures/library/compare.py
+++ tests/testfixtures/library/compare.py
@@ -4,87 +4,95 @@ from ansible.module_utils.basic import *
 from os.path import expanduser
 import itertools
 
-def __do_not_escape_between(string, start_delimiter, end_delimiter):
-    pos = 0
-    is_looking_for_start_delimiter = True
-    result = []
-    while True:
-        if is_looking_for_start_delimiter:
-            start = string.find(start_delimiter, pos)
-            if start == -1: break
-            is_looking_for_start_delimiter = False
-            pos = start + len(start_delimiter)
-        else:
-            end = string.find(end_delimiter, pos)
-            if end == -1: break
-            is_looking_for_start_delimiter = True
-            pos = end + len(end_delimiter)
-            result.extend(range(start, end + len(end_delimiter)))
-            start = None
-            end = None
-    return result
-
-_special_chars = frozenset('()[]{}?*+-|^$\\.&~# \t\n\r\v\f')
-
-def __escape(string, regex_start_delimiter, regex_end_delimiter):
-    if string == None:
-        return None
+class FileComparer(object):
     
-    do_not_escape_between = __do_not_escape_between(string, regex_start_delimiter, regex_end_delimiter)
-    s = list(string)
-    special_chars = _special_chars
-    for i, c in enumerate(string):
-        if i in do_not_escape_between:
-            continue
-        if c in special_chars:
-            s[i] = "\\" + c
-    escaped_string = ''.join(s)
-    return escaped_string \
-        .replace(regex_start_delimiter, '') \
-        .replace(regex_end_delimiter, '')
+    _special_chars = frozenset('()[]{}?*+-|^$\\.&~# \t\n\r\v\f')
 
-def _pairwise(iterable):
-    "s -> (s0, s1), (s1,s2), (s2, s3), ..., (sn, None)"
-    a, b = itertools.tee(iterable)
-    next(b, None)
-    return itertools.izip_longest(a, b, fillvalue=None)
+    def __init__(self, regex_start_delimiter, regex_end_delimiter, skip_line_pattern=None):
+        self.regex_start_delimiter = regex_start_delimiter
+        self.regex_end_delimiter = regex_end_delimiter
+        self.skip_line_pattern = skip_line_pattern
 
-def compare(file, expected_content, regex_start_delimiter, regex_end_delimiter, skip_line_pattern=None):
-    file = expanduser(file)
-    with open(file) as f:
-        content = f.read().splitlines()
-    
-    expected_lines_iter = _pairwise(expected_content.splitlines())
-    expected_line, expected_next_line = next(expected_lines_iter, (None, None))
-    result = []
-    line_number = 0
-    for line in content:
-        line_number += 1
-        escaped_expected_line = __escape(expected_line, regex_start_delimiter, regex_end_delimiter)
-        escaped_expected_next_line = __escape(expected_next_line, regex_start_delimiter, regex_end_delimiter)
-
-        if escaped_expected_line != None and re.search("^{}$".format(escaped_expected_line), line):
-            expected_line, expected_next_line = next(expected_lines_iter, (None, None))
-            continue
+    def matches(self, file, expected_content):
+        file = expanduser(file)
+        with open(file) as f:
+            content = f.read().splitlines()
         
-        if escaped_expected_next_line != None and re.search("^{}$".format(escaped_expected_next_line), line):
-            next(expected_lines_iter, (None, None))
-            expected_line, expected_next_line = next(expected_lines_iter, (None, None))
-            continue
-        
-        if expected_line == skip_line_pattern:
-            continue
-        
-        result.append("Line {}: '{}' does not match '{}'".format(line_number, line, expected_line))
+        expected_lines_iter = self._pairwise(expected_content.splitlines())
         expected_line, expected_next_line = next(expected_lines_iter, (None, None))
-        
-    while expected_line != None:
-        if expected_line == skip_line_pattern and expected_next_line == None:
-            break
-        result.append("Line {}: EOF does not match '{}'".format(line_number + 1, expected_line))
-        expected_line, expected_next_line = next(expected_lines_iter, (None, None))
+        result = []
+        line_number = 0
+        for line in content:
+            line_number += 1
+            escaped_expected_line = self.__escape(expected_line, self.regex_start_delimiter, self.regex_end_delimiter)
+            escaped_expected_next_line = self.__escape(expected_next_line, self.regex_start_delimiter, self.regex_end_delimiter)
 
-    return len(result) == 0, result
+            if escaped_expected_line != None and re.search("^{}$".format(escaped_expected_line), line):
+                expected_line, expected_next_line = next(expected_lines_iter, (None, None))
+                continue
+            
+            if escaped_expected_next_line != None and re.search("^{}$".format(escaped_expected_next_line), line):
+                next(expected_lines_iter, (None, None))
+                expected_line, expected_next_line = next(expected_lines_iter, (None, None))
+                continue
+            
+            if expected_line == self.skip_line_pattern:
+                continue
+            
+            result.append("Line {}: '{}' does not match '{}'".format(line_number, line, expected_line))
+            expected_line, expected_next_line = next(expected_lines_iter, (None, None))
+            
+        while expected_line != None:
+            if expected_line == self.skip_line_pattern and expected_next_line == None:
+                break
+            result.append("Line {}: EOF does not match '{}'".format(line_number + 1, expected_line))
+            expected_line, expected_next_line = next(expected_lines_iter, (None, None))
+
+        return len(result) == 0, result
+
+    def __escape(self, string, regex_start_delimiter, regex_end_delimiter):
+        if string == None:
+            return None
+        
+        do_not_escape_between = self.__do_not_escape_between(string, regex_start_delimiter, regex_end_delimiter)
+        s = list(string)
+        special_chars = FileComparer._special_chars
+        for i, c in enumerate(string):
+            if i in do_not_escape_between:
+                continue
+            if c in special_chars:
+                s[i] = "\\" + c
+        escaped_string = ''.join(s)
+        return escaped_string \
+            .replace(regex_start_delimiter, '') \
+            .replace(regex_end_delimiter, '')
+
+    def __do_not_escape_between(self, string, start_delimiter, end_delimiter):
+        pos = 0
+        is_looking_for_start_delimiter = True
+        result = []
+        while True:
+            if is_looking_for_start_delimiter:
+                start = string.find(start_delimiter, pos)
+                if start == -1: break
+                is_looking_for_start_delimiter = False
+                pos = start + len(start_delimiter)
+            else:
+                end = string.find(end_delimiter, pos)
+                if end == -1: break
+                is_looking_for_start_delimiter = True
+                pos = end + len(end_delimiter)
+                result.extend(range(start, end + len(end_delimiter)))
+                start = None
+                end = None
+        return result
+
+    def _pairwise(self, iterable):
+        # "s -> (s0, s1), (s1,s2), (s2, s3), ..., (sn, None)"
+        a, b = itertools.tee(iterable)
+        next(b, None)
+        return itertools.izip_longest(a, b, fillvalue=None)
+
 
 def main():
     fields = {
@@ -95,12 +103,15 @@ def main():
         "skip_line_pattern": { "required": False, "type": "str", "default": None}
     }
     module = AnsibleModule(argument_spec=fields)
-    is_matching, result = compare(
+
+    comparer = FileComparer(
+        regex_start_delimiter=module.params['regex_start_delimiter'],
+        regex_end_delimiter=module.params['regex_end_delimiter'],
+        skip_line_pattern=module.params['skip_line_pattern']
+    )
+    is_matching, result = comparer.matches(
         module.params['file'],
         module.params['with_content'],
-        module.params['regex_start_delimiter'],
-        module.params['regex_end_delimiter'],
-        module.params['skip_line_pattern']
     )
     if is_matching:
         module.exit_json(changed=False, meta=result)
--- tests/testfixtures/library/test_compare.py
+++ tests/testfixtures/library/test_compare.py
@@ -1,5 +1,13 @@
 import pytest
-from compare import compare
+from compare import FileComparer
+
+@pytest.fixture
+def sut():
+    return FileComparer(
+        regex_start_delimiter="$$ ",
+        regex_end_delimiter=" $$",
+        skip_line_pattern="..."
+    )
 
 @pytest.fixture
 def file_with_actual_content(tmp_path):
@@ -20,7 +28,7 @@ Test-Body
     file.write_text(actual_content)
     return str(file)
 
-def test_exact_match(file_with_actual_content):
+def test_exact_match(sut, file_with_actual_content):
     excepted_content = u"""\
 Date: Wed, 24 Apr 2019 00:01:35 +0200
 From: vagrant@client.localdomain
@@ -34,12 +42,12 @@ Content-Transfer-Encoding: 7bit
 
 Test-Body
 """
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == True
     assert result == []
 
-def test_does_not_match(file_with_actual_content):
+def test_does_not_match(sut, file_with_actual_content):
     excepted_content = u"""\
 Date: Wed, 24 Apr 1999 00:01:35 +0200
 From: vagrant@client.localdomain
@@ -54,13 +62,13 @@ Content-Transfer-Encoding: 7bit
 Test-Body
 """
     
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == False
     assert result[0] == "Line 1: 'Date: Wed, 24 Apr 2019 00:01:35 +0200' does not match 'Date: Wed, 24 Apr 1999 00:01:35 +0200'"
     assert result[1] == "Line 5: 'Message-ID: <5cbf8b3f.KRE6KgzO9+saDxMn%vagrant@client.localdomain>' does not match 'Message-ID: <id@client.localdomain>'"
 
-def test_match_regex(file_with_actual_content):
+def test_match_regex(sut, file_with_actual_content):
     excepted_content = u"""\
 Date: Wed, $$ \d{1,2} \w\w\w \d{4} \d\d:\d\d:\d\d $$ +0200
 From: vagrant@client.localdomain
@@ -75,12 +83,12 @@ Content-Transfer-Encoding: 7bit
 Test-Body
 """
     
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == True
     assert result == []
 
-def test_regex_does_not_match(file_with_actual_content):
+def test_regex_does_not_match(sut, file_with_actual_content):
     excepted_content = u"""\
 Date: Wed, $$ \d{1,2} Jan \d{4} \d\d:\d\d:\d\d $$ +0200
 From: vagrant@client.localdomain
@@ -95,7 +103,7 @@ Content-Transfer-Encoding: 7bit
 Test-Body
 """
     
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == False
     assert len(result) == 1
@@ -110,49 +118,51 @@ Date: Wed, 24 Apr 2019 00:01:35 +0200
     file.write_text(actual_content)
     return str(file)
 
-def test_multiple_regex_in_one_line(file_with_only_date_header):
+def test_multiple_regex_in_one_line(sut, file_with_only_date_header):
     excepted_content = u"""\
 Date: $$ \w{3} $$, 24 Apr 2019 $$ \d\d:\d\d:\d\d $$ +0200$$ .* $$
 """
     
-    is_matching, result = compare(file_with_only_date_header, excepted_content, "$$ ", " $$")
+    is_matching, result = sut.matches(file_with_only_date_header, excepted_content)
 
     assert is_matching == True
     assert result == []
 
-def test_regex_needs_an_end_delimiter_to_be_recognized_as_such(file_with_only_date_header):
+def test_regex_needs_an_end_delimiter_to_be_recognized_as_such(sut, file_with_only_date_header):
     excepted_content = u"""\
 Date: $$ .* $$, 24 Apr 2019 $$ .* $$ +0200$$ .*
 """
     
-    is_matching, result = compare(file_with_only_date_header, excepted_content, "$$ ", " $$")
+    is_matching, result = sut.matches(file_with_only_date_header, excepted_content)
 
     assert is_matching == False
     assert result[0] == r"Line 1: 'Date: Wed, 24 Apr 2019 00:01:35 +0200' does not match 'Date: $$ .* $$, 24 Apr 2019 $$ .* $$ +0200$$ .*'"
 
-def test_other_delimiters(file_with_only_date_header):
+def test_other_delimiters(sut, file_with_only_date_header):
+    sut.regex_start_delimiter = "AUF "
+    sut.regex_end_delimiter = " ZU"
     excepted_content = u"""\
 Date: AUF \w{3} ZU, 24 Apr 2019 AUF \d\d:\d\d:\d\d ZU +0200
 """
     
-    is_matching, result = compare(file_with_only_date_header, excepted_content, "AUF ", " ZU")
+    is_matching, result = sut.matches(file_with_only_date_header, excepted_content)
 
     assert is_matching == True
     assert result == []
 
-def test_skip_lines_match1(file_with_actual_content):
+def test_skip_lines_match1(sut, file_with_actual_content):
     excepted_content = u"""\
 ...
 To: test@mail-sink.theirdomain.test
 Subject: Test
 ...
 """
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$", skip_line_pattern="...")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == True
     assert result == []
 
-def test_skip_lines_match2(file_with_actual_content):
+def test_skip_lines_match2(sut, file_with_actual_content):
     excepted_content = u"""\
 ...
 To: test@mail-sink.theirdomain.test
@@ -161,12 +171,12 @@ Subject: Test
 Content-Transfer-Encoding: 7bit
 ...
 """
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$", skip_line_pattern="...")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == True
     assert result == []
 
-def test_skip_lines_match3(file_with_actual_content):
+def test_skip_lines_match3(sut, file_with_actual_content):
     excepted_content = u"""\
 ...
 To: test@mail-sink.theirdomain.test
@@ -176,12 +186,12 @@ Content-Transfer-Encoding: 7bit
 ...
 Test-Body
 """
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$", skip_line_pattern="...")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == True
     assert result == []
 
-def test_skip_lines_match4(file_with_actual_content):
+def test_skip_lines_match4(sut, file_with_actual_content):
     excepted_content = u"""\
 ...
 To: $$ .+@.+ $$
@@ -191,25 +201,25 @@ Content-Transfer-Encoding: $$ .* $$
 ...
 Test-Body
 """
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$", skip_line_pattern="...")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == True
     assert result == []
 
-def test_skip_lines_no_match1(file_with_actual_content):
+def test_skip_lines_no_match1(sut, file_with_actual_content):
     excepted_content = u"""\
 ...
 To: test@mail-sink.theirdomain.test
 Subject: Does not match
 ...
 """
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$", skip_line_pattern="...")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == False
     assert len(result) == 1
     assert result[0] == "Line 4: 'Subject: Test' does not match 'Subject: Does not match'"
 
-def test_skip_lines_no_match2(file_with_actual_content):
+def test_skip_lines_no_match2(sut, file_with_actual_content):
     excepted_content = u"""\
 ...
 To: test@mail-sink.theirdomain.test
@@ -218,7 +228,7 @@ Subject: Does not match
 Content-Transfer-Encoding: 42bit
 ...
 """
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$", skip_line_pattern="...")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == False
     # The line with the wrong "Content-Transfer-Encoding" is not matched because
@@ -228,7 +238,7 @@ Content-Transfer-Encoding: 42bit
     assert result[1] == "Line 12: EOF does not match '...'"
     assert result[2] == "Line 12: EOF does not match 'Content-Transfer-Encoding: 42bit'"
 
-def test_skip_lines_no_match3(file_with_actual_content):
+def test_skip_lines_no_match3(sut, file_with_actual_content):
     excepted_content = u"""\
 ...
 To: test@mail-sink.theirdomain.test
@@ -237,7 +247,7 @@ Subject: Test
 Content-Transfer-Encoding: 42bit
 ...
 """
-    is_matching, result = compare(file_with_actual_content, excepted_content, "$$ ", " $$", skip_line_pattern="...")
+    is_matching, result = sut.matches(file_with_actual_content, excepted_content)
 
     assert is_matching == False
     # The line with the wrong "Content-Transfer-Encoding" is not matched because
